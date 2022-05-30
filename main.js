3bd0f3eb1e091ada4a091e3833d35a300fac0167
Yasomalako <Yasomalako@gmail.com>
2022-05-30 10:36:51 +0300

    yy

--- main.js
+++ main.js
@@ -3,3 +3,15 @@
 // console.log(same,same1,same2);
 // console.log(same1);
 // console.log(same2);
+// console.log(x);
+function printInput(){
+ let button =  document.getElementById("inpu").value;
+
+
+document.write("<h2>"+ button.value +"</h2>")
+  }
+  printInput
+// function printColer(){
+
+// }let collor = window.prompt("")
+// printColer()
